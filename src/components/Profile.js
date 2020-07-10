e3867ced3e2c9bc3eda71f3e1ac36372ababa292
Benz <Benzeman097-nafazbenzema@gmail.com>
2020-07-10 21:01:35 +0530

    profile implemented

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
+import '../css/style.css';
 
 function Profile(props) {
     return (
@@ -11,7 +12,7 @@ function Profile(props) {
                      <div className="profile-nav-info">
                        <h3 className="user-name">Nafaz Benzema</h3>
                        <div className="address">
-                           <p className="city">Colombo</p>
+                           <p className="city">Colombo,</p>
                           <span className="country">
                                   Sri Lanka
                           </span>
@@ -59,16 +60,58 @@ function Profile(props) {
                              <div className="user-rating">
                                        <h3 className="rating">4.5</h3>
                                        <div className="rate">
+                                           <div className="stars">
                                            <i className="fa fa-star">
                                            </i>
                                            <i className="fa fa-star">
                                            </i>
                                            <i className="fa fa-star">
                                            </i>
+                                           </div>
+                                           <span className="no-user">
+                                                <span>
+                                                    123
+                                                </span>
+                                                &nbsp; &nbsp; reviews
+                                           </span>
                                        </div>
                              </div>
                           </div>
 
+                       </div>
+
+                       <div className="right-side">
+                           <div className="nav">
+                                  <ul>
+                                      <li onClick="tabs(0)" className="user-post-active"></li>
+                                      <li onClick="tabs(1)" className="user-review"></li>
+                                      <li onClick="tabs(2)" className="user-setting"></li>
+                                  </ul>
+                           </div>
+                           <div className="profile-body">
+                               <div className="profile-post-tab">
+                            <h1>Your Post</h1>
+                            <p>as sdsdsdds fdfddffd fhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhdddddddddddddddd dsfdddddddddddddddddddddfd
+                                dddddddddddddddddddddddddddd ffffffffffffffffffffffffffffffffff          fffffffffffffffffffff 
+                                sddddddddddddddddddddddddddddddddddddddddddd
+                            </p>
+                               </div>
+                               <div className="profile-review-tab">
+
+                               <h1>User Reviews</h1>
+                            <p>as sdsdsdds fdfddffd fhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhdddddddddddddddd dsfdddddddddddddddddddddfd
+                                dddddddddddddddddddddddddddd ffffffffffffffffffffffffffffffffff          fffffffffffffffffffff 
+                                sddddddddddddddddddddddddddddddddddddddddddd
+                            </p>
+                               </div>
+                               <div className="profile-setting-tab">
+                               <h1>Account Setting</h1>
+                            <p>as sdsdsdds fdfddffd fhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhdddddddddddddddd dsfdddddddddddddddddddddfd
+                                dddddddddddddddddddddddddddd ffffffffffffffffffffffffffffffffff          fffffffffffffffffffff 
+                                sddddddddddddddddddddddddddddddddddddddddddd
+                            </p>
+                               </div>
+                           </div>
                        </div>
              </div>
         </div>
